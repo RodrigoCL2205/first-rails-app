afd627a80cc61b549c287885ccc87a27f6a9a165
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-10-19 10:54:28 -0300

    Desafio concluido

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,12 @@ class PagesController < ApplicationController
   end
 
   def contact
+    search = params[:teacher]
+    @teachers = ['tatchi', 'gui', 're', 'mariana', 'carlos']
 
+    if search.present?
+      @teachers = @teachers.select { |teacher| teacher.downcase.start_with?(params[:teacher]) }
+    end
+    raise
   end
 end
